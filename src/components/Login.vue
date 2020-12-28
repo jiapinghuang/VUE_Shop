8f1cf6c98ac49471bccfbde2bfcd0808375968e6
jiapinghuang <1053517232@qq.com>
2020-12-28 09:04:37 +0800

    初始化项目

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+   <div id="login">
+     登陆
+   </div>
+</template>
+
+<script>
+  export default{
+     name: 'login'
+  }
+</script>
+
+<style lang="less" scoped="scoped">
+
+</style>
